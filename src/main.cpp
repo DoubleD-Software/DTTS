37f0f69bad34c56c79d4d939b7a9db4d271f49d8
tesa-klebeband <david@hoheisels-welt.de>
2023-12-27 23:10:54 +0100

    Display test message on 7-segment display in order to verify functionality of library

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 #include <fatal.h>
 #include <rfid.h>
 #include <oled.h>
+#include <tm1637.h>
 
 RFID rfid;
 OLED oled(OLED_SDA, OLED_SCL, OLED_WIDTH, OLED_HEIGHT);
+TM1637 num_disp(NUM_DISP_CLK, NUM_DISP_DIO, 6);
 
 void setup() {
     DEBUG_SER_INIT(115200);
@@ -14,9 +16,11 @@ void setup() {
 
     rfid.begin();
     oled.begin();
+    num_disp.begin();
 
     oled.clear();
     oled.print("DTTS");
+    num_disp.display_num_buffer((uint8_t*) "\1\2\3\4\5\6");
 
     DEBUG_SER_PRINTLN("Initialized DTTS.");
 }
