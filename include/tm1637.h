29169c013b7e5c3c9e0e926dd18b2058bf8a7d5f
tesa-klebeband <david@hoheisels-welt.de>
2023-12-27 23:09:12 +0100

    Add custom TM1637 7-segment library header

---
+++ include/tm1637.h
@@ -0,0 +1,54 @@
+#ifndef NUMDISP_H
+#define NUMDISP_H
+
+#include <Arduino.h>
+#include <debug.h>
+
+/***************
+   ---
+  |   |
+   ---
+  |   |
+   ---    .
+
+
+      -01-
+  20 |    | 02
+      -40-
+  10 |    | 04
+      -08-    .80
+
+*/
+
+#define TM1637_ADDR_AUTO           0x40
+#define TM1637_CMD_SET_ADDR        0xC0
+#define TM1637_CMD_DISPLAY         0x88
+#define DISPLAY_DOT                0x80
+
+class TM1637 {
+    public:
+        TM1637(int clk_pin, int dio_pin, int digits = 6);
+        void begin();
+        void setBrightness(int brightness);
+        void display_raw_buffer(uint8_t *buffer);
+        void display_num_buffer(uint8_t *buffer);
+        void display_string(const char *str);
+
+    private:
+        void display();
+        uint8_t writeByte(uint8_t data);
+        void writeSync(int pin, int value);
+        void start();
+        void stop();
+        
+        int clk_pin;
+        int dio_pin;
+        int digits;
+        int brightness = 10;
+        uint8_t *digit_order;
+        uint8_t *digit_buffer;
+        uint8_t *dec_mapping;
+        uint8_t *ascii_mapping;
+};
+
+#endif
