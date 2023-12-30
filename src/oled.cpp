ffc71c28857ca31b6a0840be9786eb0a2ea682a1
tesa-klebeband <david@hoheisels-welt.de>
2023-12-30 21:57:51 +0100

    Add code comments and documentation to oled.cpp and rfid.cpp

--- src/oled.cpp
+++ src/oled.cpp
@@ -1,5 +1,13 @@
 #include <oled.h>
 
+/**
+ * Initialize class variables during class creation when constructor gets called.
+ * In order to initialize the OLED screen, begin() has to be called.
+ * @param pin_sda The SDA pin the screen is connected to.
+ * @param pin_scl The SCL pin the screen is connected to.
+ * @param width The width of the screen in pixels.
+ * @param height The height of the screen in pixels.
+*/
 OLED::OLED(int pin_sda, int pin_scl, int width, int height) {
     this->pin_sda = pin_sda;
     this->pin_scl = pin_scl;
@@ -7,19 +15,29 @@ OLED::OLED(int pin_sda, int pin_scl, int width, int height) {
     this->height = height;
 }
 
+/**
+ * Initialize the OLED screen. This function has to be called before any other function of this class.
+ * In case of an error, the system will halt, with an error message on the serial.
+*/
 void OLED::begin() {
     DEBUG_SER_PRINTLN("Initializing OLED module...");
-    disp_wire = new TwoWire(0);
-    disp_wire->begin(pin_sda, pin_scl);
-    display = new Adafruit_SSD1306(width, height, disp_wire, -1);
+    disp_wire = new TwoWire(0); // Allocate memory for the wire object.
+    disp_wire->begin(pin_sda, pin_scl); // Initialize the wire object with the given pins.
+    display = new Adafruit_SSD1306(width, height, disp_wire, -1); // Allocate memory for the display object of type Adafruit_SSD1306.
 
-    if(!display->begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    if(!display->begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Initialize the display object with the given parameters.
         DEBUG_SER_PRINTLN(F("SSD1306 allocation failed"));
         sysHalt();
     }
     DEBUG_SER_PRINTLN("OLED module initialized.");
 }
 
+/**
+ * Clear the screen with the given color and set the cursor to the given position.
+ * @param color The color to clear the screen with.
+ * @param cursor_x The x position of the cursor.
+ * @param cursor_y The y position of the cursor.
+*/
 void OLED::clear(int color, int cursor_x, int cursor_y) {
     display->clearDisplay();
     display->setCursor(cursor_x, cursor_y);
@@ -27,6 +45,12 @@ void OLED::clear(int color, int cursor_x, int cursor_y) {
     display->setTextColor(color);
 }
 
+/**
+ * Print the given string to the screen with the given text size and color.
+ * @param str The string to print.
+ * @param text_size The text size to print the string with.
+ * @param color The color to print the string with.
+*/
 void OLED::print(const char *str, int text_size, int color) {
     display->setTextColor(color);
     display->setTextSize(text_size);
@@ -34,6 +58,11 @@ void OLED::print(const char *str, int text_size, int color) {
     display->display();
 }
 
+/**
+ * Set the cursor to the given position.
+ * @param cursor_x The x position of the cursor.
+ * @param cursor_y The y position of the cursor.
+*/
 void OLED::setCursor(int cursor_x, int cursor_y) {
     display->setCursor(cursor_x, cursor_y);
 }
--- src/rfid.cpp
+++ src/rfid.cpp
@@ -1,30 +1,39 @@
 #include <rfid.h>
 
-RFID::RFID() {
-    
+
+RFID::RFID() {   
 }
 
+/**
+ * Initialize the RFID module. This function has to be called before any other function of this class.
+*/
 void RFID::begin() {
     DEBUG_SER_PRINTLN("Initializing RFID module...");
     Serial1.begin(RFID_BAUD);
 
-    cmd_read_multi = new uint8_t[10];
+    cmd_read_multi = new uint8_t[10]; // Allocate memory for the command arrays.
     cmd_stop_multi = new uint8_t[7];
 
     const uint8_t read_multi[10] = {0XAA, 0X00, 0X27, 0X00, 0X03, 0X22, 0XFF, 0XFF, 0X4A, 0XDD};
     const uint8_t stop_multi[7] = {0XAA, 0X00, 0X28, 0X00, 0X00, 0X28, 0XDD};
 
-    memcpy(cmd_read_multi, read_multi, 10);
+    memcpy(cmd_read_multi, read_multi, 10); // Copy the command arrays to the class variables.
     memcpy(cmd_stop_multi, stop_multi, 7);
 
-    stopReadMulti();
+    stopReadMulti(); // Stop any ongoing multi read.
     DEBUG_SER_PRINTLN("RFID module initialized.");
 }
 
+/**
+ * Start the RFID module in multi read mode.
+*/
 void RFID::startReadMulti() {
     Serial1.write(cmd_read_multi, 10);
 }
 
+/**
+ * Stop the RFID module from multi read mode.
+*/
 void RFID::stopReadMulti() {
     Serial1.write(cmd_stop_multi, 7);
 }
