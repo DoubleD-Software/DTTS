06eddf1a6d50cefb5871c2cc6cac113406ad078b
tesa-klebeband <david@hoheisels-welt.de>
2023-12-31 14:10:45 +0100

    Test RFID class

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,14 @@ RFID rfid;
 OLED oled(OLED_SDA, OLED_SCL, OLED_WIDTH, OLED_HEIGHT);
 TM1637 num_disp(NUM_DISP_CLK, NUM_DISP_DIO, 6);
 
+void rfidCallback(uint8_t* tag) {
+    DEBUG_SER_PRINT("Tag: ");
+    for (int i = 0; i < 12; i++) {
+        DEBUG_SER_PRINT(tag[i], HEX);
+        DEBUG_SER_PRINT(" ");
+    }
+}
+
 void setup() {
     DEBUG_SER_INIT(115200);
     DEBUG_SER_PRINTLN("Initializing DTTS...");
@@ -23,6 +31,8 @@ void setup() {
     oled.print("DoubleD Software\n", 1);
     oled.print(VERSION, 1);
     num_disp.displayString("ddsoft,");
+
+    rfid.startReadMulti(rfidCallback);
 
     DEBUG_SER_PRINTLN("Initialized DTTS.");
 }
