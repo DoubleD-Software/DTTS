c8098d86a47e80f5160f579c910554f65fc03c82
tesa-klebeband <david@hoheisels-welt.de>
2024-01-06 15:44:31 +0100

    Add createTable and insertIntoTable functions

--- include/sql.h
+++ include/sql.h
@@ -6,6 +6,23 @@
 #include <fatal.h>
 #include <sqlite3.h>
 
+#define SQL_TYPE_INT 0
+#define SQL_TYPE_VARCHAR 1
+
+typedef struct {
+    const char *name;
+    int type;
+    int size = 255;
+    const char *additional = NULL;
+} sql_column_descriptor_t;
+
+typedef struct {
+    const char *name;
+    int type;
+    int value_int;
+    const char *value_varchar;
+} sql_column_t;
+
 class SQL {
     public:
         SQL();
@@ -14,8 +31,11 @@ class SQL {
         void closeDB();
         int clearTable(const char *table_name);
         int dropTable(const char *table_name);
+        int createTable(const char *table_name, sql_column_descriptor_t *columns, int column_count);
+        int insertIntoTable(const char *table_name, sql_column_t *columns, int column_count);
 
     private:
+        int dbExec(const char *sql);
         sqlite3 *db;
 };
 
--- src/sql.cpp
+++ src/sql.cpp
@@ -88,3 +88,120 @@ int SQL::dropTable(const char *table_name) {
         return rc;
     }
 }
+
+/**
+ * Execute a SQL statement.
+ * @param sql The SQL statement to execute.
+ * @return 0 on success, error code on failure.
+*/
+int SQL::dbExec(const char *sql) {
+    DEBUG_SER_PRINT("Executing statement: ");
+    DEBUG_SER_PRINTLN(sql);
+    char *err_msg = 0;
+    int rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    if (rc) {
+        DEBUG_SER_PRINTLN("Failed to insert into table.");
+        DEBUG_SER_PRINTLN(err_msg);
+        sqlite3_free(err_msg);
+        return rc;
+    } else {
+        DEBUG_SER_PRINTLN("Inserted into table successfully.");
+        return rc;
+    }
+}
+
+/**
+ * Create a table in the database. This function is currently vunerable to SQL injection. Has to be fixed in the future.
+ * @param table_name The name of the table to create. Has to be maximum of 128 characters long.
+ * @param columns An array of sql_column_descriptor_t structs, containing the column names, types and sizes.
+ * @param column_count The number of columns in the table.
+ * @return 0 on success, error code on failure.
+*/
+int SQL::createTable(const char *table_name, sql_column_descriptor_t *columns, int column_count) {
+    int table_name_size = strlen(table_name);
+    if (table_name_size > 128) {
+        DEBUG_SER_PRINTLN("Invalid Table Name. Name too long.");
+        return 1;
+    }
+    char sql[13 + table_name_size + 1] = {0}; // 13 characters for "CREATE TABLE ", table_name_size characters for the table name and 1 character for the null terminator.
+    sprintf(sql, "CREATE TABLE %s (", table_name);
+    for (int i = 0; i < column_count; i++) {
+        if (i != 0) {
+            strcat(sql, ", ");
+        }
+        strcat(sql, columns[i].name);
+        switch (columns[i].type) {
+            case SQL_TYPE_INT: {
+                strcat(sql, " INT");
+                break;
+            }
+            case SQL_TYPE_VARCHAR: {
+                strcat(sql, " VARCHAR(");
+                char size[6] = {0};
+                sprintf(size, "%d", columns[i].size);
+                strcat(sql, size);
+                strcat(sql, ")");
+                break;
+            }
+            default: {
+                DEBUG_SER_PRINTLN("Invalid column type.");
+                return 1;
+            }
+        }
+        if (columns[i].additional != NULL) {
+            strcat(sql, " ");
+            strcat(sql, columns[i].additional);
+        }
+    }
+    strcat(sql, ")");
+    dbExec(sql);
+}
+
+/**
+ * Insert a row into the given table in the database. This function is currently vunerable to SQL injection. Has to be fixed in the future.
+ * @param table_name The name of the table to insert into. Has to be maximum of 128 characters long.
+ * @param columns An array of sql_column_t structs, containing the column names, types and values.
+ * @param column_count The number of columns in the table.
+ * @return 0 on success, error code on failure.
+*/
+int SQL::insertIntoTable(const char *table_name, sql_column_t *columns, int column_count) {
+    int table_name_size = strlen(table_name);
+    if (table_name_size > 128) {
+        DEBUG_SER_PRINTLN("Invalid Table Name. Name too long.");
+        return 1;
+    }
+    char sql[13 + table_name_size + 1] = {0}; // 13 characters for "INSERT INTO ", table_name_size characters for the table name and 1 character for the null terminator.
+    sprintf(sql, "INSERT INTO %s (", table_name);
+    for (int i = 0; i < column_count; i++) {
+        if (i != 0) {
+            strcat(sql, ", ");
+        }
+        strcat(sql, columns[i].name);
+    }
+    strcat(sql, ") VALUES (");
+    for (int i = 0; i < column_count; i++) {
+        if (i != 0) {
+            strcat(sql, ", ");
+        }
+        switch (columns[i].type) {
+            case SQL_TYPE_INT: {
+                char value_int[11] = {0};
+                sprintf(value_int, "%d", columns[i].value_int);
+                strcat(sql, value_int);
+                break;
+            }
+            case SQL_TYPE_VARCHAR: {
+                strcat(sql, "'");
+                strcat(sql, columns[i].value_varchar);
+                strcat(sql, "'");
+                break;
+            }
+            default: {
+                DEBUG_SER_PRINTLN("Invalid column type.");
+                return 1;
+            }
+        }
+    }
+    strcat(sql, ")");
+    dbExec(sql);
+}
